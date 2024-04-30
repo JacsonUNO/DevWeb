c56a19621c42c6f6ce3d572b8d20e38d71cda74b
Jacson <jacsonerthal@unochapeco.edu.br>
2024-04-29 21:10:52 -0300

    Implementação do delete dos comentários

--- front/src/paginas/Post.js
+++ front/src/paginas/Post.js
@@ -1,24 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 const Post = () => {
     const { postId } = useParams();
-    const [post, setListaPosts] = useState([]);
-    const [replies, setListaReplies] = useState([]);
+    const [post, setPost] = useState([]);
+    const [replies, setReplies] = useState([]);
     const [reply, setReply] = useState('');
-    React.useEffect(() => {
-        const res = axios.get("blog/api/v1/rest/post/" + postId);
-        res.then((query) => {
-            setListaPosts(query.data);
-            console.log(query.data);
-        })
-        const res2 = axios.get("blog/api/v1/rest/postreply/" + postId);
-        res2.then((query) => {
-            setListaReplies(query.data);
-            console.log(query.data);
-        })
-    }, []);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await axios.get(`blog/api/v1/rest/post/${postId}`);
+            setPost(res.data);
+
+            const res2 = await axios.get(`blog/api/v1/rest/postreply/${postId}`);
+            setReplies(res2.data);
+        };
+
+        fetchData();
+    }, [postId]);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -26,7 +27,7 @@ const Post = () => {
             if (reply.length < 1) {
                 alert('Não é possível criar um comentário vazio.');
             } else {
-                const response = await axios.post('blog/api/v1/rest/reply', {
+                await axios.post('blog/api/v1/rest/reply', {
                     id_post: postId,
                     reply,
                 });
@@ -34,12 +35,20 @@ const Post = () => {
                 setReply('');
                 window.location.reload();
             }
-
         } catch (error) {
-            console.log(error);
+            console.error('Error submitting reply:', error);
         }
-    }
+    };
 
+    const handleDeleteReply = async (replyId) => {
+        try {
+            await axios.delete(`blog/api/v1/rest/reply/${replyId}`);
+            const updatedReplies = replies.filter(reply => reply.id !== replyId);
+            setReplies(updatedReplies);
+        } catch (error) {
+            console.error('Error deleting reply:', error);
+        }
+    };
 
     return (
         <div>
@@ -50,26 +59,31 @@ const Post = () => {
                     <h5>Post #{post.id}, created {post.createdAt},
                         updated {post.updatedAt}</h5>
                     <div className='row'>
-                        {replies.length > 0 && (<table className='table'>
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Resposta</th>
-                                    <th>Criado</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {replies &&
-                                    replies.map((reply, index) => (
-                                        <tr key={index}>
-                                            <th key={reply.id}>{reply.id}</th>
-                                            <th key={reply.reply}>{reply.reply}</th>
-                                            <th key={reply.createdAt}>{reply.createdAt}</th>
+                        {replies.length > 0 && (
+                            <table className='table'>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Resposta</th>
+                                        <th>Criado</th>
+                                        <th>Ações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {replies.map((reply) => (
+                                        <tr key={reply.id}>
+                                            <td>{reply.id}</td>
+                                            <td>{reply.reply}</td>
+                                            <td>{reply.createdAt}</td>
+                                            <td>
+                                                <button onClick={() => handleDeleteReply(reply.id)}>
+                                                    Excluir
+                                                </button>
+                                            </td>
                                         </tr>
-                                    ))
-                                }
-                            </tbody>
-                        </table>
+                                    ))}
+                                </tbody>
+                            </table>
                         )}
                         <form onSubmit={handleSubmit}>
                             <div>
@@ -80,15 +94,14 @@ const Post = () => {
                                 />
                             </div>
                             <div>
-                                <button type='submit' style={{marginLeft: 93 }}>Criar comentário</button>
+                                <button type='submit' style={{ marginLeft: 93 }}>Criar comentário</button>
                             </div>
                         </form>
                     </div>
                 </div>
             )}
         </div>
-    )
-
-}
+    );
+};
 
 export default Post;
